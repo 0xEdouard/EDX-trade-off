df3fb92ee756466677b2ae46232394ae061cafa6
Edouard <Edouard.Verstraete@UGent.be>
2024-11-28 11:00:55 +0100

    add bram

--- app/api/traders/route.ts
+++ app/api/traders/route.ts
@@ -5,6 +5,7 @@ const traders: TraderInfo[] = [
   { name: 'DIESEL', apiKey: process.env.DIESEL_API_KEY!, apiSecret: process.env.DIESEL_API_SECRET! },
   { name: 'EDUARDO', apiKey: process.env.EDUARDO_API_KEY!, apiSecret: process.env.EDUARDO_API_SECRET! },
   { name: 'VANPA', apiKey: process.env.VANPA_API_KEY!, apiSecret: process.env.VANPA_API_SECRET! },
+  { name: 'BRAMVDE', apiKey: process.env.BRAMVDE_API_KEY!, apiSecret: process.env.BRAMVDE_API_SECRET! },
 ].filter(trader => trader.apiKey && trader.apiSecret);
 
 export async function GET() {
