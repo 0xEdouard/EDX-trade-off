40349b0ee991264eb0056ceb95bca206061a2a42
Edouard <Edouard.Verstraete@UGent.be>
2024-12-11 17:50:34 +0100

    fix

--- app/api/traders/route.ts
+++ app/api/traders/route.ts
@@ -1,27 +1,31 @@
-import { NextResponse } from 'next/server'
-import { fetchTraderData, TraderInfo } from '../../lib/bybit-api'
-import { getAllUsers } from '@/app/lib/db';
-import { User } from '@/app/types/User';
-
+import { NextResponse } from "next/server";
+import { fetchTraderData, TraderInfo } from "../../lib/bybit-api";
+import { getAllUsers } from "@/app/lib/db";
+import { User } from "@/app/types/User";
 
 let users: User[] = [];
 let traders: TraderInfo[] = [];
 
-await getAllUsers().then((users_db: User[]) => {
-  users = users_db;
-  traders = users.map((user: User) => ({
-    name: user.name,
-    apiKey: user.api_key,
-    apiSecret: user.api_secret
-  }));
-});
+if (process.env.NEXT_PHASE !== "PHASE_PRODUCTION_BUILD") {
+  await getAllUsers().then((users_db: User[]) => {
+    users = users_db;
+    traders = users.map((user: User) => ({
+      name: user.name,
+      apiKey: user.api_key,
+      apiSecret: user.api_secret,
+    }));
+  });
+}
 
 export async function GET() {
   try {
     const traderData = await Promise.all(traders.map(fetchTraderData));
-    return NextResponse.json(traderData)
+    return NextResponse.json(traderData);
   } catch (error) {
-    console.error('Error fetching trader data:', error)
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    console.error("Error fetching trader data:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+import { PHASE_PRODUCTION_BUILD } from "next/constants";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PHASE:
+      process.env.NODE_ENV === "production" ? PHASE_PRODUCTION_BUILD : "",
+  },
 };
 
 export default nextConfig;
