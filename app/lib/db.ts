7fd666a1fcbdd04224e6052ba91150affee60b4e
Edouard <Edouard.Verstraete@UGent.be>
2024-12-11 17:12:37 +0100

    updates

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,28 +2,28 @@ import { Pool } from "pg";
 import { User } from "../types/User";
 
 const pool = new Pool({
-    user: "postgres",
-    host: "51.20.135.233",
-    database: "edx",
-    password: "eduroam",
-    port: 5432
+  host: "127.0.0.1",
+  port: 5432,
+  database: "edx",
+  user: "postgres",
+  password: "eduroam",
 });
 
-// DAO Functions
-
 export const getAllUsers = async (): Promise<User[]> => {
-  const result = await pool.query("SELECT * FROM \"user\";");
+  const result = await pool.query('SELECT * FROM "User";');
   return result.rows;
 };
 
 export const getUserById = async (id: string): Promise<User | null> => {
-  const result = await pool.query("SELECT * FROM \"user\" WHERE id = $1;", [id]);
+  const result = await pool.query('SELECT * FROM "User" WHERE id = $1;', [id]);
   return result.rows[0] || null;
 };
 
-export const getWalletBalanceHistoryByUserId = async (userId: string): Promise<any[]> => {
+export const getWalletBalanceHistoryByUserId = async (
+  userId: string
+): Promise<any[]> => {
   const result = await pool.query(
-    `SELECT * FROM balenciaga
+    `SELECT * FROM Balenciaga
      WHERE user_id = $1
      ORDER BY timestamp ASC;`,
     [userId]
@@ -42,4 +42,3 @@ export const addWalletBalanceHistory = async (
     [userId, balance, timestamp]
   );
 };
-
